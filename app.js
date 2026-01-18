219d7274ece831acdca9e0bfb21a6357d7fe872b
Ganesh Devde <gannupatil19@gmail.com>
2026-01-18 19:37:55 +0530

    app route updated

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ async function main() {
   await mongoose.connect(dbUrl);
 }
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
